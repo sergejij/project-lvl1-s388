dbbd80f82520e555f1bfdf5e11d6c070d2aa752d
sergejij <ushakoff.sergej2010@yandex.ru>
2018-12-04 12:24:43 +0400

    Step two is ready, correct

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
 import userName from '..';
 
+console.log("Welcome to the Brain Games!");
 userName();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-console.log("Welcome to the Brain Games!");
+
 export default () => {
   const actual = readlineSync.question('May I have your name? ');
   console.log('Hello, ' + actual + '!');
