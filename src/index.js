066c521689a6dfd8105e818bb341c90a98e36d72
sergejij <ushakoff.sergej2010@yandex.ru>
2018-12-05 20:29:57 +0400

    step 4, correct4

--- src/index.js
+++ src/index.js
@@ -14,20 +14,21 @@ export const userAnswer = () => {
   console.log(`Hello, ${name}!\n`);
 
   const iter = (win) => {
-    if (win > 3) {
+    const threeTime = 3;
+    if (win > threeTime) {
       console.log(`Congratulations, ${name}!`);
       return;
     }
-    const number = Math.floor((Math.random() * (100 - 1)) + 1);
-    const actual = readlineSync.question(`Question:  ${number} `);
+    const numberQuestion = Math.floor((Math.random() * (100 - 1)) + 1);
+    const answer = readlineSync.question(`Question:  ${numberQuestion} `);
 
-    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
-    if (actual === correctAnswer) {
+    const correctAnswer = numberQuestion % 2 === 0 ? 'yes' : 'no';
+    if (answer === correctAnswer) {
       console.log('Correct!');
       iter(win + 1);
       return;
     }
-    console.log(`${actual} is wrong answer ;(. Correct answer was ${correctAnswer}. \n Let's try again, ${name}!`);
+    console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}. \n Let's try again, ${name}!`);
   };
   iter(1);
 };
