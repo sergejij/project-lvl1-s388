4d999b54b74219a2b7caca4b5a2941416367e82d
sergejij <ushakoff.sergej2010@yandex.ru>
2018-12-06 21:19:36 +0400

    calc

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,53 +1,53 @@
 import readlineSync from 'readline-sync';
 import generateNum from '../utils';
 
-const threeTime = 3;
+const stopIfMore = 3;
 const calculate = () => {
   console.log('Welcome to the Brain Games!');
   console.log('What is the result of the expression?\n');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!\n`);
 
-  const iter = (win) => {
+  const iter = (countUserWins) => {
     const numberOperand = Math.floor((Math.random() * (4 - 1)) + 1);
-    const number1 = generateNum();
-    const number2 = generateNum();
+    const randomNum1 = generateNum();
+    const randomNum2 = generateNum();
 
-    if (win > threeTime) {
+    if (countUserWins > stopIfMore) {
       console.log(`Congratulations, ${name}!`);
       return;
     }
 
-    const correctAnswerPlus = number1 + number2;
-    const correctAnswerMinus = number1 - number2;
-    const correctAnswerMultiply = number1 * number2;
+    const correctAnswerPlus = randomNum1 + randomNum2;
+    const correctAnswerMinus = randomNum1 - randomNum2;
+    const correctAnswerMultiply = randomNum1 * randomNum2;
 
     if (numberOperand === 1) {
-      const answer = readlineSync.question(`Question:  ${number1} + ${number2} \nYour answer: `);
-      if (Number.parseInt(answer, 10) === correctAnswerPlus) {
+      const userAnswer = readlineSync.question(`Question:  ${randomNum1} + ${randomNum2} \nYour answer: `);
+      if (Number.parseInt(userAnswer, 10) === correctAnswerPlus) {
         console.log('Correct!');
-        iter(win + 1);
+        iter(countUserWins + 1);
         return;
       }
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswerPlus}. \n`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswerPlus}. \n`);
       console.log(`Let's try again, ${name}!`);
     } else if (numberOperand === 2) {
-      const answer = readlineSync.question(`Question:  ${number1} - ${number2} \nYour answer: `);
-      if (Number.parseInt(answer, 10) === correctAnswerMinus) {
+      const userAnswer = readlineSync.question(`Question:  ${randomNum1} - ${randomNum2} \nYour answer: `);
+      if (Number.parseInt(userAnswer, 10) === correctAnswerMinus) {
         console.log('Correct!');
-        iter(win + 1);
+        iter(countUserWins + 1);
         return;
       }
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswerMinus}. \n`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswerMinus}. \n`);
       console.log(`Let's try again, ${name}!`);
     } else {
-      const answer = readlineSync.question(`Question:  ${number1} * ${number2} \nYour answer: `);
-      if (Number.parseInt(answer, 10) === correctAnswerMultiply) {
+      const userAnswer = readlineSync.question(`Question:  ${randomNum1} * ${randomNum2} \nYour answer: `);
+      if (Number.parseInt(userAnswer, 10) === correctAnswerMultiply) {
         console.log('Correct!');
-        iter(win + 1);
+        iter(countUserWins + 1);
         return;
       }
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswerMultiply}. \n`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswerMultiply}. \n`);
       console.log(`Let's try again, ${name}!`);
     }
   };
